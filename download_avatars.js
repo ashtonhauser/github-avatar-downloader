8b23449e2660993126d1d795d8e5faf782e98c86
Ashton Hauser <ashton.hauser777@gmail.com>
2018-10-29 23:10:37 +0000

    Altered git ignore to not include private token. Enabled download_avatars.js ability to apply token.

--- download_avatars.js
+++ download_avatars.js
@@ -1,1 +1,23 @@
 var request = require('request');
+var tokens = require('./secrets');
+
+console.log('Welcome to Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner +  '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + tokens.GITHUB_TKN 
+    }
+  };
+
+  request(url, function(err, response, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors('jquery', 'jquery', function (err, result) {
+  console.log('Error:', err);
+  console.log('Result:', result);
+});
